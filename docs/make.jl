8eb1afab3c33dd1ecd2c0c14353ccfca13da56d2
jeffersonfparil <jeffersonparil@gmail.com>
2025-01-24 10:04:19 +1100

    struggling to fix opengl headless mode

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,6 @@
+using Pkg
+# Pkg.develop(PackageSpec(path=pwd()))
+Pkg.instantiate()
 using GBPlots
 using Documenter
 
--- docs/makedocs_for_ci.jl
+++
@@ -1,5 +0,0 @@
-using Pkg
-cd("..")
-# Pkg.develop(PackageSpec(path=pwd()))
-Pkg.instantiate()
-include("make.jl")
