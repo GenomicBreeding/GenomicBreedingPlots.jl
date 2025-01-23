994517d11ef337d9743de750a5d0f8a47931ce38
jeffersonfparil <jeffersonparil@gmail.com>
2025-01-23 12:53:44 +1100

    fixing opengl headless mode

--- src/phenomes.jl
+++ src/phenomes.jl
@@ -328,7 +328,7 @@ phenomes.phenotypes[1, 1] = missing
 f = plotinteractive2d(phenomes)
 ```
 """
-function plotinteractive2d(
+function GBPlots.plotinteractive2d(
     phenomes::Phenomes;
     idx_entries::Union{Nothing,Vector{Int64}} = nothing,
     idx_traits::Union{Nothing,Vector{Int64}} = nothing,
@@ -438,7 +438,8 @@ function plotinteractive2d(
     df[!, :pc1] = M.proj[:, 1]
     df[!, :pc2] = M.proj[:, 2]
     # Include the 2 PCs in the list of traits
-    traits = sort(names(df)[4:end])
+    traits = names(df)[4:end]
+    traits = traits[sortperm(uppercase.(traits))]
     # Activate Makie using the OpenGL plotting backend
     GLMakie.activate!()
     GLMakie.closeall() # close any open screen
@@ -454,7 +455,7 @@ function plotinteractive2d(
             traits[1]
         end
         menu_trait_x = Menu(fig, options = traits, default = default_trait_x)
-        tb_x = Textbox(fig, placeholder = "Search trait 1")
+        tb_x = Textbox(fig)
         # Set the trait 2 selector
         default_trait_y = if length(traits) > 2
             "pc2"
@@ -462,7 +463,7 @@ function plotinteractive2d(
             traits[2]
         end
         menu_trait_y = Menu(fig, options = traits, default = default_trait_y)
-        tb_y = Textbox(fig, placeholder = "Search trait 2")
+        tb_y = Textbox(fig)
         # Instantiate Pearson's correlation value per pair of traits
         ρ = Observable(string(round(cor(df[!, default_trait_x], df[!, default_trait_y]), digits = 4)))
         # Place these trait menus in a left sidebar
@@ -476,38 +477,10 @@ function plotinteractive2d(
             Label(fig, @lift("ρ = $($ρ)"));
             tellheight = false,
         )
-
-        on(tb_x.stored_string) do s
-            bool_matches = .!isnothing.(match.(Regex(s), traits))
-            if sum(bool_matches) > 0
-                idx = findall(bool_matches)[1]
-                menu_trait_x.selection[] = traits[idx]
-                menu_trait_x.i_selected[] = idx
-                menu_trait_x.is_open[] = true
-            end
-        end
-        on(tb_y.stored_string) do s
-            bool_matches = .!isnothing.(match.(Regex(s), traits))
-            if sum(bool_matches) > 0
-                idx = findall(bool_matches)[1]
-                menu_trait_y.selection[] = traits[idx]
-                menu_trait_y.i_selected[] = idx
-                menu_trait_y.is_open[] = true
-            end
-        end
-
-
-
-
-
-
-
-
-
         # Place the main scatter plot with histograms for the 2 traits
         fig_main = fig[1:2, 2] = GridLayout()
         plot_hist_x = Axis(fig_main[1, 1])
-        plot_scatter = Axis(fig_main[2, 1], xlabel = traits[1], ylabel = traits[2])
+        plot_scatter = Axis(fig_main[2, 1], xlabel = default_trait_x, ylabel = default_trait_y)
         plot_hist_y = Axis(fig_main[2, 2])
 
         plot_heatmap = Axis(
@@ -532,8 +505,8 @@ function plotinteractive2d(
         )
 
 
-        X = Observable{Any}(df[!, traits[1]])
-        Y = Observable{Any}(df[!, traits[2]])
+        X = Observable{Any}(df[!, default_trait_x])
+        Y = Observable{Any}(df[!, default_trait_y])
 
         for pop in populations
             idx = findall(df.populations .== pop)
@@ -558,8 +531,35 @@ function plotinteractive2d(
         GLMakie.hidedecorations!(plot_hist_y, grid = false)
         leg.tellheight = true
 
+
+        # Reactivity stuff
+        # Search-ish functionality-ish
+        on(tb_x.stored_string) do s
+            bool_matches = .!isnothing.(match.(Regex(s, "i"), traits)) # case-insensitive with the Regex flag "i"
+            if sum(bool_matches) > 0
+                traits_reordered = vcat(traits[bool_matches], traits[.!bool_matches])
+                menu_trait_x.is_open = true
+                menu_trait_x.selection = traits_reordered[1]
+                menu_trait_x.i_selected = 1
+                menu_trait_x.options = traits_reordered
+            else
+                menu_trait_x.options = traits
+            end
+        end
+        on(tb_y.stored_string) do s
+            bool_matches = .!isnothing.(match.(Regex(s, "i"), traits)) # case-insensitive with the Regex flag "i"
+            if sum(bool_matches) > 0
+                traits_reordered = vcat(traits[bool_matches], traits[.!bool_matches])
+                menu_trait_y.is_open = true
+                menu_trait_y.selection = traits_reordered[1]
+                menu_trait_y.i_selected = 1
+                menu_trait_y.options = traits_reordered
+            else
+                menu_trait_y.options = traits
+            end
+        end
+        # Drop down menus
         on(menu_trait_x.selection) do s
-            # trait_x[] = s
             X[] = df[!, s]
             plot_scatter.xlabel = s
             autolimits!(plot_scatter)
