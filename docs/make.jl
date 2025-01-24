0968a5cdd9dac396a1886508dbfae765f7995cb1
jeffersonfparil <jeffersonparil@gmail.com>
2025-01-24 14:13:54 +1100

    struggling to fix opengl headless mode

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,7 @@
 using Pkg
 Pkg.add(url="https://github.com/GenomicBreeding/GBCore.jl")
 Pkg.add(url="https://github.com/GenomicBreeding/GBPlots.jl")
+Pkg.add(Documenter)
 using GBPlots
 using Documenter
 
