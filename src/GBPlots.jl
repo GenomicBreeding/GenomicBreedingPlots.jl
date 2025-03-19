5157f8fe760453fb884955b91bd498341f05d77c
jeffersonfparil <jeffersonparil@gmail.com>
2025-03-20 08:35:57 +1100

    removing PrecompileTools.jl

--- src/GBPlots.jl
+++ src/GBPlots.jl
@@ -5,7 +5,6 @@ using StatsBase, MultivariateStats, Distributions, LinearAlgebra
 using DataFrames, Random
 using Distances, Clustering, Measures
 using CairoMakie, ColorSchemes
-# using PrecompileTools: @compile_workload
 
 include("commons.jl")
 include("genomes.jl")
@@ -17,9 +16,5 @@ include("cv.jl")
 export PlotsGB, DistributionPlots, ViolinPlots, CorHeatPlots, TreePlots, BarPlots, BoxPlots, PCBiPlots
 export checkdims, labeltofname, saveplots
 export plot
-
-# @compile_workload begin
-#     true
-# end
 
 end
