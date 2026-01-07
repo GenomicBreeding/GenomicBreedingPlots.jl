f2326eb2729c8bd97cabf378afd4b77e13186fe2
jeffersonfparil <jeffersonparil@gmail.com>
2026-01-07 13:16:07 +1100

    updated GBCore and releasing v0.2.0

--- test/interactive_prelude.jl
+++ test/interactive_prelude.jl
@@ -1,10 +1,10 @@
 using Pkg
 Pkg.activate(".")
-try
-    Pkg.update()
-catch
-    nothing
-end
+# try
+#     Pkg.update()
+# catch
+#     nothing
+# end
 using GenomicBreedingPlots
 using GenomicBreedingCore
 using StatsBase, MultivariateStats, Distributions, LinearAlgebra
